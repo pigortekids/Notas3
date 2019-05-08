bcbcfa7bf6f369f8c7d71bb2e330008f2b36221a
Igor Correa <igor.a.correa@hotmail.com>
2019-05-07 21:28:16 -0300

    mais pronto ainda

--- Nuvem/projeto/html/app/index.js
+++ Nuvem/projeto/html/app/index.js
@@ -1,21 +1,6 @@
-/*
-document.getElementById("butaumLivros").addEventListener("click", function(){
-    var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-			var jaison = JSON.parse(xhttp.responseText);
-			console.log(xhttp.responseText)
-		}
-	};
-	xhttp.open("GET", "http://localhost:8081/public/index.php/v1/livro");
-	//xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send();
-});
-*/
-
-document.getElementById("butaumLivros").addEventListener("click", function(){
-    var colunas = ["Nome", "Autor", "Genero"];
-    geraTabela("livro", colunas);
+document.getElementById('butaumLivros').addEventListener('click', function(){
+    var colunas = ['Nome', 'Autor', 'Genero'];
+    geraTabela('livro', colunas);
 });
 
 
@@ -24,8 +9,9 @@ function geraTabela(elemento, colunas)
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         //if (this.readyState == 4 && this.status == 200) {
+		if (xhttp.responseText != ''){
             var jaison = JSON.parse(xhttp.responseText);
-            var tabela = document.getElementById("tabela");
+            var tabela = document.getElementById('tabela');
 
             //limpa linhas e colunas da tabela
             while (tabela.rows.length > 0){
@@ -33,36 +19,36 @@ function geraTabela(elemento, colunas)
             }
 
             //adiciona primeira linha
-            var tr = document.createElement("tr");
+            var tr = document.createElement('tr');
             tabela.appendChild(tr);
 
             //adiciona e edita todos os valores da tabela
             for(i = 0; i < colunas.length; i++){
                 var td = document.createElement('td');
                 tr.appendChild(td);
-                tabela.getElementsByTagName("td")[i].innerHTML = colunas[i].toUpperCase();
-                tabela.getElementsByTagName("td")[i].style.textAlign = "center";
-                tabela.getElementsByTagName("td")[i].style.width = "148px";
-                tabela.getElementsByTagName("td")[i].style.height = "24px";
-                tabela.getElementsByTagName("td")[i].style.font = "normal normal 14px Special Elite,display";
+                tabela.getElementsByTagName('td')[i].innerHTML = colunas[i].toUpperCase();
+                tabela.getElementsByTagName('td')[i].style.textAlign = 'center';
+                tabela.getElementsByTagName('td')[i].style.width = '148px';
+                tabela.getElementsByTagName('td')[i].style.height = '24px';
+                tabela.getElementsByTagName('td')[i].style.font = 'normal normal 14px Special Elite,display';
             }
 
             for (i in jaison){
                 var linha = tabela.insertRow(-1);
                 for(j = 0; j < colunas.length; j++){
                     var celula = linha.insertCell(j);
-                    celula.style.textAlign = "center";
-                    celula.style.height = "30px";
-                    celula.style.font = "normal normal 12px Special Elite,display";
-                    var key = colunas[j].toLowerCase().replace(" ", "_");
+                    celula.style.textAlign = 'center';
+                    celula.style.height = '30px';
+                    celula.style.font = 'normal normal 12px Special Elite,display';
+                    var key = colunas[j].toLowerCase().replace(' ', '_');
                     var texto  = document.createTextNode(jaison[i][key]);
                     celula.appendChild(texto);
                 }
             }
-        //}
+        }
     };
 
-    xhttp.open("GET", "http://localhost:8081/public/index.php/v1/livro");
-    //xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.open('GET', 'http://localhost:8081/public/index.php/v1/livro');
+    //xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.send();
 }
